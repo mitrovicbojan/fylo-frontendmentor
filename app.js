a894c8424143be428fa92d5afdf5d0ebb3a58082
unknown <mitrovicbojan87@gmail.com>
2023-02-12 18:23:10 +0200

    js2

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const submitBtn = document.getElementById("submit-btn");
 const invalidEmail = document.getElementById("invalidEmail");
 
 submitBtn.addEventListener("click", function () {
-  if (
-    !emailInput.value.match(
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    )
-  ) {
+  const emailValidation =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!emailInput.value.match(emailValidation)) {
     invalidEmail.innerHTML = "Please enter a valid email address.";
     return true;
   } else {
